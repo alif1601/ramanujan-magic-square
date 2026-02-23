a2979a70b9ad718ffe4c782789aae098c0e0e737
Alif Rabbi <142160858+alif1601@users.noreply.github.com>
2026-02-23 12:32:29 +0600

    sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "ramanujan-magic-square-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./404.html",
+  "./robots.txt",
+  "./sitemap.xml",
+  "./site.webmanifest",
+  "./assets/favicon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
